e7116c53bc443e994cb8a5319f0a74fdcf861c08
selphur <artiom.kuritsin@gmail.com>
2019-10-23 23:07:31 +0300

    +new class structure

--- src/main/java/com/javaguru/shoppinglist/ProductValidator.java
+++ src/main/java/com/javaguru/shoppinglist/ProductValidator.java
@@ -4,37 +4,37 @@ import java.math.BigDecimal;
 
 public class ProductValidator {
     public boolean validateNameLength(String name) {
-        if (name.length() >= Product.nameLengthMin && name.length() <= Product.nameLengthMax) {
+        if (name.length() >= Product.NAME_LENGTH_MIN && name.length() <= Product.NAME_LENGTH_MAX) {
             return true;
         } else {
-            new ValidationException("The name must be between " + Product.nameLengthMin + " and " + Product.nameLengthMax + " characters long. Please try again.");
+            new ValidationException("The name must be between " + Product.NAME_LENGTH_MIN + " and " + Product.NAME_LENGTH_MAX + " characters long. Please try again.");
             return false;
         }
     }
 
     public boolean validatePrice(BigDecimal price) {
-        if (price.compareTo(Product.priceMin) == 1) {
+        if (price.compareTo(Product.PRICE_MIN) == 1) {
             return true;
         } else {
-            new ValidationException("The price must be higher than " + Product.priceMin + ". Please try again.");
+            new ValidationException("The price must be higher than " + Product.PRICE_MIN + ". Please try again.");
             return false;
         }
     }
 
     public boolean validatePriceMinForDiscount(BigDecimal price) {
-        if(price.compareTo(Product.priceMinForDiscount) != -1) {
+        if(price.compareTo(Product.PRICE_MIN_FOR_DISCOUNT) != -1) {
             return true;
         } else {
-            System.out.println("The price is lower than " + Product.priceMinForDiscount + ", the discount cannot be set. Proceeding...");
+            System.out.println("The price is lower than " + Product.PRICE_MIN_FOR_DISCOUNT + ", the discount cannot be set. Proceeding...");
             return false;
         }
     }
 
     public boolean validateDiscount(BigDecimal discount) {
-        if ((discount.compareTo(Product.discountMin) != -1) && (discount.compareTo(Product.discountMax) != 1)) {
+        if ((discount.compareTo(Product.DISCOUNT_MIN) != -1) && (discount.compareTo(Product.DISCOUNT_MAX) != 1)) {
             return true;
         } else {
-            new ValidationException("The discount must be between " + Product.discountMin + " and " + Product.discountMax + ". Please try again.");
+            new ValidationException("The discount must be between " + Product.DISCOUNT_MIN + " and " + Product.DISCOUNT_MAX + ". Please try again.");
             return false;
         }
     }
