70c8db034a02c1e07fbaab3acc67a7292120daec
Artiom Kuritsin <artiom.kuritsin@gmail.com>
2019-10-29 21:14:27 +0200

    +validation tests;

--- src/test/java/com/javaguru/shoppinglist/ProductValidatorTest.java
+++ src/test/java/com/javaguru/shoppinglist/ProductValidatorTest.java
@@ -16,22 +16,23 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class ProductValidatorTest {
 
+    @InjectMocks
     private ProductValidator victim = new ProductValidator();
 
     @Test
-    public void nameLengthShouldReturnTrue() {
+    public void nameLengthExpectTrue() {
         boolean result = victim.validateNameLength("apple");
         assertTrue(result == true);
     }
 
     @Test
-    public void priceShouldReturnTrue() {
+    public void priceExpectTrue() {
         boolean result = victim.validatePrice(new BigDecimal(1));
         assertTrue(result == true);
     }
 
     @Test
-    public void discountShouldReturnTrue() {
+    public void discountExpectTrue() {
         for(int i = 0; i <= 100; i++) {
             boolean result = victim.validateDiscount(new BigDecimal(i));
             assertTrue(result == true);
@@ -39,10 +40,27 @@ public class ProductValidatorTest {
     }
 
     @Test
-    public void priceMinForDiscountShouldReturnFalse() {
+    public void priceMinForDiscountExpectFalse() {
         for(int i = 0; i < 20; i++) {
             boolean result = victim.validatePriceMinForDiscount(new BigDecimal(i));
-            assertEquals(false,result);
+            assertEquals(false, result);
         }
+    }
+
+    @Mock
+    ProductRepository productRepositoryMock;
+
+    @Test
+    public void validateNameUniqueExpectFalse() {
+        Map<Long, Product> productRepository = new HashMap<>();
+
+        Product product = new Product();
+        product.setName("apple");
+        productRepository.put(0L, product);
+
+        when(productRepositoryMock.getProductRepository()).thenReturn(productRepository);
+
+        boolean result = victim.validateNameUnique("apple", productRepositoryMock);
+        assertEquals(false, result);
     }
 }
